935e6bf42cacd519f68673dc289c30b3423e50ce
nakama5211 <nakama@gmail.com>
2017-09-22 11:27:03 +0700

    nam-form edit
    zcxv

--- application/views/home/master.php
+++ application/views/home/master.php
@@ -11,6 +11,9 @@ if($this->session->has_userdata('user')) {
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/main.css">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Vali Admin</title>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries-->
     <!--if lt IE 9
--- application/views/page/list_bieumau.php
+++ application/views/page/list_bieumau.php
@@ -20,6 +20,7 @@
                       
                       <th>Tên biểu mẫu</th>
                       <th>File mẫu</th>
+                      <th>Mô tả</th>
                       <th>Dữ liệu</th>
                       <th>Xem chi tiết</th>
                       <th>Sửa</th>
@@ -31,9 +32,10 @@
                          
                           <td><?php echo $bieumau->ten ?></td>
                           <td><?php echo $bieumau->file ?></td>
+                          <td><?php echo $bieumau->mota ?></td>
                           <td><?php echo $bieumau->dulieu ?></td>
                           <td><a class="btn btn-info btn-flat" href="<?php echo base_url().'chitiet_bieumau/'.$bieumau->id ?>"><i class="fa fa-lg fa-eye"></i></a></td>
-                          <td><a class="btn btn-primary btn-flat" href="#"><i class="fa fa-lg fa-pencil"></i></a></td>
+                          <td><a class="btn btn-primary btn-flat" data-toggle="modal" href="#edit"><i class="fa fa-lg fa-pencil"></i></a></td>
                         </tr>                    
                     <?php endforeach;?>
                   </tbody>
@@ -43,3 +45,40 @@
           </div>
         </div>
       </div>
+        <div class="modal fade" id="edit">
+           <div class="modal-dialog">
+           <div class="modal-content">
+              <div class="modal-header">
+                 <button type="button" class="close" data-dismiss='modal' aria-hidden="true"><span class="glyphicon glyphicon-remove"></span></button>
+                 <h4 class="modal-title" style="font-size: 32px; padding: 12px;"> Recover Your Password </h4>
+              </div>
+              <form method="post" action="forgot-password">
+              <input type="hidden" name="_token" value="">
+              <div class="modal-body">
+                 <div class="container-fluid">
+                    <div class="row">
+                       <div class="col-xs-12 col-sm-12 col-md-12">
+                          <div class="form-group">
+                             <div class="input-group">
+                                <div class="input-group-addon iga2">
+                                   <span class="glyphicon glyphicon-envelope"></span>
+                                </div>
+                                <input type="email" class="form-control" placeholder="Enter Your E-Mail " name="email">
+                             </div>
+                          </div>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+
+              <div class="modal-footer">
+                 <div class="form-group">
+                    <button type="submit" class="btn btn-lg btn-info"> Save <span class="glyphicon glyphicon-saved"></span></button>
+
+                    <button type="button" data-dismiss="modal" class="btn btn-lg btn-default"> Cancel <span class="glyphicon glyphicon-remove"></span></button>
+                 </div>
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
