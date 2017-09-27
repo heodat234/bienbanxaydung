34cc6c7d7d3aef9d759bf350e5433d9703897273
heodat234 <thanhhung23495@gmail.com>
2017-09-27 11:15:51 +0700

    hung

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -8,6 +8,7 @@ class export extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
         $this->load->helper('date');
+        $this->load->helper('My_helper');
         // Load the Library
         $this->load->library("excel");
         $this->load->library('session');
@@ -34,7 +35,7 @@ class export extends CI_Controller {
         $objPHPExcel->setActiveSheetIndex(0);
 
         $dulieu = $this->Bienban_model->get_dulieu_id($id);
-        $filename = $this->stripUnicode($dulieu->ten_bienban);
+        $filename = stripUnicode($dulieu->ten_bienban);
         $dulieu = unserialize($dulieu->dulieu);
         // var_dump($filename); 
 
@@ -80,7 +81,7 @@ class export extends CI_Controller {
             $objPHPExcel->setActiveSheetIndex(0);
 
             $dulieu = $this->Bienban_model->get_dulieu_id($da->id);
-            $filename = $this->stripUnicode($dulieu->ten_bienban);
+            $filename = stripUnicode($dulieu->ten_bienban);
             $dulieu = unserialize($dulieu->dulieu);
             // var_dump($dulieu[8]);        
             foreach ($dulieu as $dl) {
@@ -124,18 +125,5 @@ class export extends CI_Controller {
 
 
 
-    public function stripUnicode($str){
-      if(!$str) return false;
-       $unicode = array(
-          'a'=>'á|à|ả|ã|ạ|ă|ắ|ặ|ằ|ẳ|ẵ|â|ấ|ầ|ẩ|ẫ|ậ',
-          'd'=>'đ',
-          'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
-          'i'=>'í|ì|ỉ|ĩ|ị',
-          'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',
-          'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',
-          'y'=>'ý|ỳ|ỷ|ỹ|ỵ',
-       );
-    foreach($unicode as $nonUnicode=>$uni) $str = preg_replace("/($uni)/i",$nonUnicode,$str);
-    return $str;
-    }
+    
 }
